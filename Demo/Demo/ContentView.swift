b46a1201b9b73f917bfa690e0b7dd6dda47bf6af
Sergii Khliustin <51409210+sergeykhliustin@users.noreply.github.com>
2024-09-09 19:02:26 +0200

    MACD indicator (#12)

--- Demo/Demo/ContentView.swift
+++ Demo/Demo/ContentView.swift
@@ -11,7 +11,8 @@ struct ContentView: View {
                 MAIndicator(),
             ],
             secondaryContent: [
-                RSIIndicator()
+                RSIIndicator(),
+                MACDIndicator()
             ]
         )
     }
