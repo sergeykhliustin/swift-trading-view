7f522a56d9c910819ccd645b6ab0c35f8f265b02
Sergii Khliustin <sergey.khliustin@gmail.com>
2026-02-11 13:52:42 +0300

    bump ta-lib. bump swift version

--- Sources/SwiftTradingView/ScrollOffsetPreferenceKey.swift
+++ Sources/SwiftTradingView/ScrollOffsetPreferenceKey.swift
@@ -2,7 +2,7 @@ import Foundation
 import SwiftUI
 
 struct ScrollOffsetPreferenceKey: PreferenceKey {
-    static var defaultValue: CGFloat = 0
+    static let defaultValue: CGFloat = 0
     static func reduce(value: inout CGFloat, nextValue: () -> CGFloat) {
         value = nextValue()
     }
